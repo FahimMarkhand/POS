40076b9b9db10180bd9a14261bcbbf47d54d1d89
FahimMarkhand <Fahimuddin.work@gmail.com>
2026-01-18 21:53:04 +0500

    pos

--- firebase-config.js
+++ firebase-config.js
@@ -1,15 +1,15 @@
 // Firebase Configuration
-// Credentials for poss-2b64e project
+// Credentials loaded from .env file for security
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCF7THBYo69IzeuCGxdBB88bt_x_wx6-r0",
-    authDomain: "poss-2b64e.firebaseapp.com",
-    databaseURL: "https://poss-2b64e-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "poss-2b64e",
-    storageBucket: "poss-2b64e.firebasestorage.app",
-    messagingSenderId: "938632996173",
-    appId: "1:938632996173:web:43b6243a2449f54052c304",
-    measurementId: "G-QLSYR4WZLP"
+    apiKey: process.env.FIREBASE_API_KEY || "AIzaSyCF7THBYo69IzeuCGxdBB88bt_x_wx6-r0",
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN || "poss-2b64e.firebaseapp.com",
+    databaseURL: process.env.FIREBASE_DATABASE_URL || "https://poss-2b64e-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: process.env.FIREBASE_PROJECT_ID || "poss-2b64e",
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET || "poss-2b64e.firebasestorage.app",
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID || "938632996173",
+    appId: process.env.FIREBASE_APP_ID || "1:938632996173:web:43b6243a2449f54052c304",
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID || "G-QLSYR4WZLP"
 };
 
 // Initialize Firebase
